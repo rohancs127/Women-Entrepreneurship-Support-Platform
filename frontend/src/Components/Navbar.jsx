87150d09007bba9602f993d462edd599b4f45b1c
Rohan <rohankumar5803@gmail.com>
2024-03-07 21:35:35 +0530

    navbar created

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Search} from 'lucide-react';
+import '../styles/Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar-section'>
+        <h1 className='page-title'>ShePreneur</h1>
+        <div className='navigate-and-search-section'>
+            <div className='navigate-section'>
+                <ul>Home</ul>
+                <ul>Mentors</ul>
+                <ul>Investment</ul>
+            </div>
+            <div className='search-section'>
+                <Search />
+                <input className='search-input' type='text' placeholder='Search'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
