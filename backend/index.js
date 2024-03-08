e3230f2b5b989ef24cf792ad6086915fdbb7b1e8
Rohan <rohankumar5803@gmail.com>
2024-03-08 13:38:14 +0530

    database connected to backend

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,24 @@
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import express from "express";
+const express = require("express");
+const { pool } = require("./config/db");
 
 const app = express();
-const PORT = 5000;
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: "*" }));
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
+
+async function getData() {
+  const data = await pool.query("select * from demo", (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(res.rows);
+    }
+  });
+}
+
+getData();
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import '../styles/Home.css';
+import React from "react";
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className='home-section'>
-        <div className='image-list'>
-            <img src='image1.jpg'/>
-            <img src='image2.jpg'/>
-            {/* <img src='image3.jpg'/> */}
-            <img src='image4.jpg'/>
-            <img src='image5.jpg'/>
-            <img src='image6.jpg'/>
-        </div>
-        <div className='content-section'>
-            <p>Welcome to ShePreneur, your dynamic space dedicated to nurturing and celebrating the entrepreneurial spirit of women worldwide. Here, you'll find a supportive community, valuable resources, and expert guidance to help you turn your business dreams into reality.</p>
-            <p>Join us at ShePreneur, where we believe that every woman has the potential to build her empire. Whether you're a seasoned entrepreneur or just starting out, our platform offers the tools, connections, and inspiration you need to thrive in the competitive business world. Unlock your full potential and embark on your entrepreneurial journey with us today!</p>
-        </div>
-        <button className='register-button'>Register</button>
+    <div className="home-section">
+      <div className="image-list">
+        <img src="image1.jpg" />
+        <img src="image2.jpg" />
+        {/* <img src='image3.jpg'/> */}
+        <img src="image4.jpg" />
+        <img src="image5.jpg" />
+        <img src="image6.jpg" />
+      </div>
+      <div className="content-section">
+        <p>
+          Welcome to ShePreneur, your dynamic space dedicated to nurturing and
+          celebrating the entrepreneurial spirit of women worldwide. Here,
+          you'll find a supportive community, valuable resources, and expert
+          guidance to help you turn your business dreams into reality.
+        </p>
+        <p>
+          Join us at ShePreneur, where we believe that every woman has the
+          potential to build her empire. Whether you're a seasoned entrepreneur
+          or just starting out, our platform offers the tools, connections, and
+          inspiration you need to thrive in the competitive business world.
+          Unlock your full potential and embark on your entrepreneurial journey
+          with us today!
+        </p>
+      </div>
+      <button className="register-button">Login</button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
