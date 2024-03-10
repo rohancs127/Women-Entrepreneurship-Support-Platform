64ed9ad4b8eb8dfd3e9314616f9360d4bcabcf00
Rohan <rohankumar5803@gmail.com>
2024-03-10 11:06:25 +0530

    login page added

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import "../styles/Login.css";
+import { useState } from "react";
 
 function Login() {
+  const [user, setUser] = useState("Entrepreneur");
   return (
     <div className="parent-div">
       <div className="login-section">
-        <h1 className="login-heading">Login</h1>
+        <h1 className="login-heading">{user}</h1>
         <div className="users-div">
-          <a className="user-link">Entrepreneur</a>
-          <a className="user-link">Mentor</a>
-          <a className="user-link">Investor</a>
+          <a onClick={() => setUser("Entrepreneur")} className="user-link">
+            Entrepreneur
+          </a>
+          <a onClick={() => setUser("Mentor")} className="user-link">
+            Mentor
+          </a>
+          <a onClick={() => setUser("Investor")} className="user-link">
+            Investor
+          </a>
         </div>
         <div className="login-input-div">
-          <h2>Name</h2>
+          <h2 className="input-heading">{user} Name</h2>
           <input placeholder="username" />
-          <h2>UserId</h2>
-          <input placeholder="user id" />
+          <h2 className="input-heading">{user} UserId</h2>
+          <input placeholder="userid" />
         </div>
+        <button className="login-button">Login</button>
+      </div>
+      <div className="new-register">
+        New User? <a className="new-register-tag"> Register</a>
       </div>
     </div>
   );
