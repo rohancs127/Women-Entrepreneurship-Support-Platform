e212c3e53cd0af12bb3dae4133d857ba28086f5a
Rohan <rohankumar5803@gmail.com>
2024-03-18 22:27:03 +0530

    save role in local storage

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import "../styles/Login.css";
 import UserToggle from "../Components/UserToggle";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
   const [user, setUser] = useState("Entrepreneur");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("role", user);
+    navigate("/entrepreneur_page");
+  };
+
   return (
     <div className="parent-div">
       <div className="login-section">
@@ -14,19 +23,20 @@ function Login() {
           <UserToggle setUser={setUser} />
         </div>
         <div className="login-input-div">
-          <h2 className="input-heading">{user} Name</h2>
-          <input placeholder="username" className="login-input" />
+          <h2 className="input-heading">{user} Email</h2>
+          <input placeholder="email" className="login-input" />
           <h2 className="input-heading">{user} UserId</h2>
           <input placeholder="userid" className="login-input" />
         </div>
-        <Link to="/entrepreneur_page">
-          <button className="login-button">Login</button>
-        </Link>
+
+        <button className="login-button" onClick={handleSubmit}>
+          Login
+        </button>
       </div>
       <div className="new-register">
         New User?{" "}
         <Link to="/registration">
-          <a className="new-register-tag"> Register</a>
+          <p className="new-register-tag"> Register</p>
         </Link>
       </div>
     </div>
